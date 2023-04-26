8718c4e382219daf9f12e2b30f4fe989223cc29d
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2023-04-25 17:23:28 -0700

    64-bit struct changes

--- pkg/graphics/stplot/psikern/psi.h
+++ pkg/graphics/stplot/psikern/psi.h
@@ -55,9 +55,9 @@ define  PSI_XRES        Memi[$1+6]              # device resolution in X
 define  PSI_YRES        Memi[$1+7]              # device resolution in Y
 define  PSI_ZRES        Memi[$1+8]              # device resolution in Z
 define  PSI_FILLSTYLE   Memi[$1+9]              # number of fill styles
-define  PSI_LWSLOPE     Memr[$1+10]             # Difference in line widths in
+define  PSI_LWSLOPE     Memr[P2R($1+10)]        # Difference in line widths in
                                                 # GKI units.
-define  PSI_LWORIGIN    Memr[$1+11]             # Line widht in GKI units of
+define  PSI_LWORIGIN    Memr[P2R($1+11)]        # Line widht in GKI units of
                                                 # a line of width 1.
 define  PSI_CURSOR      Memi[$1+12]             # last cursor accessed
 define  PSI_COLOR       Memi[$1+13]             # last color set
@@ -67,13 +67,13 @@ define  PSI_TYPE        Memi[$1+16]             # last line type set
 define  PSI_WIDTH       Memi[$1+17]             # last line width set
 define  PSI_DEVNAME     Memi[$1+18]             # name of open device
 define  PSI_PORTRAIT    Memb[$1+19]             # True to print out in portait.
-define  PSI_XSIZE       Memr[$1+20]             # Page size along X (meters)
-define  PSI_YSIZE       Memr[$1+21]             # Page size along Y (meters)
-define  PSI_XOFF        Memr[$1+22]             # Page offset along X (meters).
-define  PSI_YOFF        Memr[$1+23]             # Page offset along Y (meters).
+define  PSI_XSIZE       Memr[P2R($1+20)]        # Page size along X (meters)
+define  PSI_YSIZE       Memr[P2R($1+21)]        # Page size along Y (meters)
+define  PSI_XOFF        Memr[P2R($1+22)]        # Page offset along X (meters).
+define  PSI_YOFF        Memr[P2R($1+23)]        # Page offset along Y (meters).
 define  PSI_PROLOG      Memi[$1+24]             # PostScrip prolog file desc.
 define  PSI_UP          Memi[$1+25]             # Last text angle.
-define  PSI_GKI2OUT     Memr[$1+26]             # Scale conversion from GKI
+define  PSI_GKI2OUT     Memr[P2R($1+26)]        # Scale conversion from GKI
                                                 # to a reduced output space.
 define  PSI_DASH        Memi[$1+27]             # Size of a dash in output space
 define  PSI_DOT         Memi[$1+28]             # Size of dot in output space.
@@ -82,14 +82,14 @@ define  PSI_PATH        Memi[$1+30]             # Last text path set.
 define  PSI_HJUSTIFY    Memi[$1+31]             # Last horizontal justification.
 define  PSI_VJUSTIFY    Memi[$1+32]             # Last verticle justification.
 define  PSI_VARIABLE    Memi[$1+33]             # Last state of variable-spaced.
-define  PSI_SPACING     Memr[$1+34]             # Space relative to height.
+define  PSI_SPACING     Memr[P2R($1+34)]        # Space relative to height.
 define	PSI_BACK	Memi[$1+35]		# Background color.
 define	PSI_DOCELL	Memi[$1+36]		# Draw cell arrays?
 define	PSI_DOFILL	Memi[$1+37]		# Do area fills?
         # extra space
 define  PSI_CHARHEIGHT  Memi[$1+50+$2-1]        # character height
 define  PSI_CHARWIDTH   Memi[$1+60+$2-1]        # character width
-define  PSI_CHARSIZE    Memr[$1+70+$2-1]        # text sizes permitted
+define  PSI_CHARSIZE    Memr[P2R($1+70+$2-1)]   # text sizes permitted
 define  PSI_PLAP        ($1+80)                 # polyline attributes
 define  PSI_PMAP        ($1+84)                 # polymarker attributes
 define  PSI_FAAP        ($1+88)                 # fill area attributes
@@ -119,7 +119,7 @@ define  TX_STATE        Memi[$1]
 define  TX_UP           Memi[$1+1]
 define  TX_SIZE         Memi[$1+2]
 define  TX_PATH         Memi[$1+3]
-define  TX_SPACING      Memr[$1+4]
+define  TX_SPACING      Memr[P2R($1+4)]
 define  TX_HJUSTIFY     Memi[$1+5]
 define  TX_VJUSTIFY     Memi[$1+6]
 define  TX_FONT         Memi[$1+7]
